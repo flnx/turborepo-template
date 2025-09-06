760ebc16890619d8e7365fa5930a9e02f8c16f45
flnx <flnx@users.noreply.github.com>
2025-09-06 09:16:52 +0200

    supabase client is initialized per instance, not request. Habits restructure

--- apps/api/src/plugins/app/supabase.ts
+++ apps/api/src/plugins/app/supabase.ts
@@ -1,4 +1,3 @@
-import { SupabaseClient } from '@supabase/supabase-js';
 import { FastifyInstance } from 'fastify';
 import fp from 'fastify-plugin';
 
@@ -7,25 +6,13 @@ import { createSupabaseClient } from '@/utils/createSupabaseClient';
 import type { CreateSupabaseClient } from '@/utils/createSupabaseClient';
 
 declare module 'fastify' {
-  export interface FastifyRequest {
+  export interface FastifyInstance {
     supabase: CreateSupabaseClient;
   }
 }
 
-/**
- * Initiliazes the supabase client for each request
- *
- * - If the user is authenticated, it attaches the supabase client to each request (req.supabase)
- */
-
 async function supabasePlugin(app: FastifyInstance) {
-  app.decorateRequest('supabase', null as unknown as SupabaseClient);
-
-  app.addHook('onRequest', async (req) => {
-    // Validation unnecessary. The header 100% exists, because the authorization plugin already did the validation
-    const userAccessToken = req.headers.authorization!;
-    req.supabase = createSupabaseClient(userAccessToken);
-  });
+  app.decorate('supabase', createSupabaseClient());
 }
 
 export default fp(supabasePlugin, {
